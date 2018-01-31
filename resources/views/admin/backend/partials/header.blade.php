8968d3dbb16d54757efb807fbf468201816d9c87
kenntjm12 <kenntjm12@gmail.com>
2018-01-31 14:36:54 -0400

    agregando imagenes de prueba

--- resources/views/admin/backend/partials/header.blade.php
+++ resources/views/admin/backend/partials/header.blade.php
@@ -3,10 +3,13 @@
   @include('admin.backend.partials.navbar')
   <!--end navbar-->
 
+
   <div class="jumbotron jumbotron-fluid">
+
     <div class="container">
-      <h1 class="display-4">Quejate Ya!!</h1>
-      <p class="lead">Publica tus quejas que ocurren en tu ciudad.</p>
+  
+    <h1 class="display-4">Quejate Ya!!</h1>
+    <p class="lead">Publica tus quejas que ocurren en tu ciudad.</p>
     </div>
   </div>
 </header>
